49f941f51d731abfd4aa9ba90066339202ebe646
Wopsie <joaghim.beltman@gmail.com>
2015-12-15 13:46:36 +0100

    dingding

---
+++ BerendTest/Assets/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour 
+{
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //if player is in range
+            //attack
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10f);
+        int i = 0;
+        while (i < hitColliders.Length) 
+        {
+            hitColliders[i].SendMessage("AddDamage");
+            i++;
+        
+        }
+
+        Gizmos.DrawSphere(transform.position, 10f);
+	}
+}

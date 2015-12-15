44c93600908d5e79ebd809adf912e4cdb142ad4c
jimbo333 <19198@ma-web.nl>
2015-12-15 13:43:36 +0100

    PlayerMovement

---
+++ BerendTest/Assets/script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 10.0F;
+	[SerializeField]
+	private float rotationSpeed = 100.0F;
+
+	// Update is called once per frame
+	void Update()
+	{
+		PlayerMove();
+	}
+	
+	void PlayerMove()
+	{
+		float translation = Input.GetAxis("Vertical") * speed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+	}
+}

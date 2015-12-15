23276d1b3be892ccb2cfaa82f448112b6093badc
Ezowolf <wiggler@live.nl>
2015-12-15 13:50:00 +0100

    Lel

---
+++ BerendTest/Assets/Scripts/ChaseWhenClose.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseWhenClose : MonoBehaviour {
+	[SerializeField]
+	private int distanceStartingChasing = 1;
+
+	[SerializeField]
+	private GameObject objectToFollow;
+
+	private bool shouldIChase = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position,objectToFollow.transform.position)<=distanceStartingChasing)
+		{
+			shouldIChase = true;
+		}
+		else
+		{
+			shouldIChase = false;
+		}
+		if(shouldIChase)
+		{
+			transform.LookAt(objectToFollow.transform.position);
+			transform.Translate(Vector3.forward * Time.deltaTime);
+		}
+		else
+		{
+			//Als je niet meer hoeft te chasen
+		}
+	}
+}

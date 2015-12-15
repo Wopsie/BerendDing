19ca1091c464c2f7a1107e3de66fa0d6a10370d7
Wopsie <joaghim.beltman@gmail.com>
2015-12-15 13:59:03 +0100

    added "ATTACK THE BASTERD"

--- BerendTest/Assets/Scripts/Attack.cs
+++ BerendTest/Assets/Scripts/Attack.cs
@@ -1,24 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attack : MonoBehaviour 
+public class Attack : MonoBehaviour
 {
-	
-	// Update is called once per frame
-	void Update () 
+
+    void OnCollisionEnter(Collision coll)
     {
-        //if player is in range
-            //attack
-
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10f);
-        int i = 0;
-        while (i < hitColliders.Length) 
+        if (coll.gameObject.tag == "Player")
         {
-            hitColliders[i].SendMessage("AddDamage");
-            i++;
-        
+            Debug.Log("Attack the Basterd");
         }
+    }
 
-        Gizmos.DrawSphere(transform.position, 10f);
-	}
+    
 }
